39a3248b04d7721252ada176671aa105c23f9fdd
Scott Biggs <scott.biggs88@gmail.com>
2024-02-13 22:36:10 -0500

    [runs} Updated python script to match excel tool

--- src/main/py/ProcessPath.py
+++ src/main/py/ProcessPath.py
@@ -1,17 +1,16 @@
 import json, os
+import pdb
 
 #open our file
 
-working_path = "C:\Users\scott\Documents\Source\C_Source\RaiderBot_2024\RaiderBot_2024\src\main\pathing\Output_Directory/"
+#pdb.set_trace()
+working_path = "C:/Users/scott/Documents/Source/C_Source/RaiderBot_2024/RaiderBot_2024/src/main/pathing/Output_Directory/"
 working_name = "TestPath1"
 
-output_path = "C:\Users\scott\Documents\Source\C_Source\RaiderBot_2024\RaiderBot_2024\src\main\include\MotionProfiles/"
+output_path = "C:/Users/scott/Documents/Source/C_Source/RaiderBot_2024/RaiderBot_2024/src/main/include/MotionProfiles/"
 output_name = working_name + ".hpp"
 
 json_file = working_path + working_name + ".wpilib.json"
-
-rotation_start = 0
-rotation_end = 90
 
 with open(json_file) as json_data:
     data = json.load(json_data) 
@@ -26,7 +25,7 @@ for elm in data:
     time_final = str(elm["time"])
     time_size += 1
 
-time_head = "const double " + working_name + "_T" + "[" + str(time_size) + "] = {"
+time_head = "const double KnADAS_t_" + working_name + "[" + str(time_size) + "] = {"
 time_foot = "};"
 
 time_output = time_head + time_data + time_foot
@@ -37,22 +36,21 @@ time_remain_data = ""
 rot_size = 0
 
 for elm in data:
-   data_deg = (elm["pose"]["rotation"]["radians"] * (180/3.14159265))
+   data_deg = (elm["holonomicRotation"])
    rot_data += str(data_deg) + ","
    rot_size += 1
 
-# rot_data = str(rotation_start) + "," + str(rotation_end)
 for elm in data:
     time_remain_data += str(float(time_final) - float(elm["time"])) + ","
     
 
 
-rot_head = "const double " + working_name + "_ROT" + "[" + str(rot_size) + "] = {"
+rot_head = "const double KaADAS_Deg_" + working_name + "[" + str(rot_size) + "] = {"
 rot_foot = "};"
 
 rot_output = rot_head + rot_data + rot_foot
 
-time_remain_head = "const double " + working_name + "_T_REM" + "[" + str(rot_size) + "] = {"
+time_remain_head = "const double KaADAS_t_" + working_name + "Remaining" + "[" + str(rot_size) + "] = {"
 rot_time_output = time_remain_head + time_remain_data + rot_foot
 
 #x
@@ -60,11 +58,11 @@ x_data = ""
 x_size = 0
 
 for elm in data:
-    data_in = (elm["pose"]["translation"]["x"] * (39.37008))
+    data_in = (elm["pose"]["translation"]["y"] * (39.37008))
     x_data += str(data_in) + ","
     x_size += 1
 
-x_head = "const double " + working_name + "_X" + "[" + str(x_size) + "] = {"
+x_head = "const double KaADAS_l_" + working_name + "_X" + "[" + str(x_size) + "] = {"
 x_foot = "};"
 
 x_output = x_head + x_data + x_foot
@@ -74,11 +72,11 @@ y_data = ""
 y_size = 0
 
 for elm in data:
-    data_in = (elm["pose"]["translation"]["y"] * (39.37008))
+    data_in = (elm["pose"]["translation"]["x"] * (39.37008))
     y_data += str(data_in) + ","
     y_size += 1
 
-y_head = "const double " + working_name + "_Y" + "[" + str(y_size) + "] = {"
+y_head = "const double KaADAS_l_" + working_name + "_Y" + "[" + str(y_size) + "] = {"
 y_foot = "};"
 
 y_output = y_head + y_data + y_foot
