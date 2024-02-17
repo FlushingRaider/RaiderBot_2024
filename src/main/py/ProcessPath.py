bbefee4158542375a25e8b65f04795085832d530
AlphaDragon601 <50785295+AlphaDragon601@users.noreply.github.com>
2024-02-17 10:25:26 -0500

    small change: ProcessPath.py now outputs vectors :)

--- src/main/py/ProcessPath.py
+++ src/main/py/ProcessPath.py
@@ -4,10 +4,10 @@ import pdb
 #open our file
 
 #pdb.set_trace()
-working_path = "C:/Users/gnich/Documents/RaiderBot_2024/src/main/pathing/Output_Directory/"
+working_path = "C:/Users/alpha/Documents/GitHub/2024/RaiderBot_2024/src/main/pathing/Output_Directory/"
 working_name = "Precision_Test"
 
-output_path = "C:/Users/gnich/Documents/RaiderBot_2024/src/main/include/MotionProfiles/"
+output_path = "C:/Users/alpha/Documents/GitHub/2024/RaiderBot_2024/src/main/include/MotionProfiles/"
 output_name = working_name + ".hpp"
 
 json_file = working_path + working_name + ".wpilib.json"
@@ -25,7 +25,7 @@ for elm in data:
     time_final = str(elm["time"])
     time_size += 1
 
-time_head = "const double KnADAS_t_" + working_name + "[" + str(time_size) + "] = {"
+time_head = "const std::vector<double> KnADAS_t_" + working_name + " = {"
 time_foot = "};"
 
 time_output = time_head + time_data + time_foot
@@ -45,12 +45,12 @@ for elm in data:
     
 
 
-rot_head = "const double KaADAS_Deg_" + working_name + "[" + str(rot_size) + "] = {"
+rot_head = "const std::vector<double> KaADAS_Deg_" + working_name + " = {"
 rot_foot = "};"
 
 rot_output = rot_head + rot_data + rot_foot
 
-time_remain_head = "const double KaADAS_t_" + working_name + "Remaining" + "[" + str(rot_size) + "] = {"
+time_remain_head = "const std::vector<double> KaADAS_t_" + working_name + "Remaining" + " = {"
 rot_time_output = time_remain_head + time_remain_data + rot_foot
 
 #x
@@ -62,7 +62,7 @@ for elm in data:
     x_data += str(data_in) + ","
     x_size += 1
 
-x_head = "const double KaADAS_l_" + working_name + "_X" + "[" + str(x_size) + "] = {"
+x_head = "const std::vector<double> KaADAS_l_" + working_name + "_X" + " = {"
 x_foot = "};"
 
 x_output = x_head + x_data + x_foot
@@ -76,12 +76,13 @@ for elm in data:
     y_data += str(data_in) + ","
     y_size += 1
 
-y_head = "const double KaADAS_l_" + working_name + "_Y" + "[" + str(y_size) + "] = {"
+y_head = "const std::vector<double> KaADAS_l_" + working_name + "_Y" + " = {"
 y_foot = "};"
 
 y_output = y_head + y_data + y_foot
 
 with open(output_path + output_name, "a") as f:
+    print("#include <vector>\n\n\n", file=f)
     print(time_output, file=f)
     print(rot_time_output, file=f)
     print(rot_output, file=f)
