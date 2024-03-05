dccc52a74f849ff61027e6f0d34b5c65c79d0c80
Chris B <115883974+ChrisB5561@users.noreply.github.com>
2024-03-04 22:33:21 -0500

    changed order

--- src/main/cpp/ADAS_DJ.cpp
+++ src/main/cpp/ADAS_DJ.cpp
@@ -336,8 +336,8 @@ case E_ADAS_DM_DJ_Opt4Path1:
 case E_ADAS_DM_DJ_Opt4Path2:
 case E_ADAS_DM_DJ_Opt4Path3:
 case E_ADAS_DM_DJ_Opt4Path4:
-  LeADAS_e_AutonRequestStateSPK = E_SPK_Ctrl_Intake;
   LeADAS_e_AutonRequestStateAMP = E_DJ_Amp_Init;
+  LeADAS_e_AutonRequestStateSPK = E_SPK_Ctrl_Intake;
   break;
 
  case E_ADAS_DM_DJ_Opt1Path1:
